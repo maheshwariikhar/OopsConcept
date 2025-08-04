540e9d03d6a87a5b5046a9a359301e46ddd0b980
Maheshwari Ikhar <maheshwariikhar@gmail.com>
2025-08-04 22:53:42 +0530

    Adding Constructor files

---
+++ OopsConcept/CopyConstructor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class CopyConstructorExample
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public string Newname { get; set; }
+        public int NewAge { get; set; }
+
+        // parameterized Constructor
+        public CopyConstructorExample(string Name, int Age)
+        {
+            name = Name;
+            age = Age;
+
+        }
+        // copy Constructor
+        public CopyConstructorExample(CopyConstructorExample copy)
+        {
+            Newname = copy.name;
+            NewAge = copy.age;
+
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name:{name}, Age:{age}");
+
+        }
+        public void DisplayInfoNew()
+        {
+            Console.WriteLine("Copy Constructor......");
+            Console.WriteLine($"Name:{Newname}, Age:{NewAge}");
+        }
+        
+    }
+
+    internal class CopyConstructor
+    {
+        static void Main()
+        {
+            CopyConstructorExample obj = new CopyConstructorExample("Mahi", 25);
+            obj.DisplayInfo();
+
+            CopyConstructorExample obj2 = new CopyConstructorExample(obj);
+            obj2.DisplayInfoNew();
+
+
+        }
+    }
+}
---
+++ OopsConcept/ParameterizedConstructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ParameterizedConstructorExp
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public ParameterizedConstructorExp( string Name, int Age)
+        {
+            name = Name;
+            age = Age;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Parameterized Constructor.....");
+            Console.WriteLine($"Name:{name}, Age:{age}");
+        }
+    }
+    internal class ParameterizedConstructor
+    {
+        static void Main()
+        {
+            ParameterizedConstructorExp p1 = new ParameterizedConstructorExp("Mahi", 25);
+            p1.DisplayInfo();
+        }
+    }
+}
