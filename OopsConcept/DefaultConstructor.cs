c8a1b58263a6e2273aa45aba23edf772d7916ffb
Maheshwari Ikhar <maheshwariikhar@gmail.com>
2025-08-04 22:53:42 +0530

    parametrized constructor exp

---
+++ OopsConcept/DefaultConstructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class DefaultConstructorExamp
+    {
+        public string name;
+        public int age;
+
+        //  Default Constructor 
+        public DefaultConstructorExamp()
+        {
+            name = "Mahi";
+            age = 25;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Default Constructor......");
+            Console.WriteLine($"Name:{name}, Age:{age}");
+        }
+    }
+    internal class DefaultConstructor
+    {
+        static void Main()
+        {
+            DefaultConstructorExamp d1 = new DefaultConstructorExamp();
+            d1.Display();
+
+        }
+    }
+}
