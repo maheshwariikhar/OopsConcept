c6ac2abf3fb74501e89790f1888e5c7e996bb3ad
Maheshwari Ikhar <maheshwariikhar@gmail.com>
2025-08-06 20:20:33 +0530

    Adding Delegates Example Assign

---
+++ OopsConcept/MultiDelegates.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public delegate void Arith(int a, int b);
+    
+    class Calculator1
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition is:" +(a + b));
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("Substraction is:" + (a - b));
+        }
+        public void Multi(int a, int b)
+        {
+            Console.WriteLine("Multiplication:" + (a * b));
+        }
+        public void Div(int a, int b)
+        {
+            Console.WriteLine("Division is:" + (a / b));
+        }
+    }
+    internal class MultiDelegates
+    {
+        static void Main()
+        {
+            Calculator1 cals = new Calculator1();
+            Arith a1 = cals.Add;
+            a1 += cals.Sub;
+            a1 += cals.Multi;
+            a1 += cals.Div;
+            a1(20, 15);
+
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ OopsConcept/SingleDelegates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public delegate void Add(int a, int b);
+    public delegate void Sub(int a, int b);
+
+    class Calculator
+    {
+        public void Addnumber(int a, int b)
+        {
+            Console.WriteLine("Addition is:" +(a + b));
+        }
+        public void Subnumber(int a, int b)
+        {
+            Console.WriteLine("Substraction is:" +(a - b));
+        }
+    }
+    internal class SingleDelegates
+    {
+        static void Main()
+        {
+            Calculator cal = new Calculator();
+            Add ad = new Add(cal.Addnumber);
+            ad(20, 10);
+            Sub sb = new Sub(cal.Subnumber);
+            sb(30,22);
+
+
+
+        }
+    }
+}
