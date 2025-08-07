de765629b3cf7bfed48ced5debcc605d7b494d7b
Maheshwari Ikhar <maheshwariikhar@gmail.com>
2025-08-07 20:37:33 +0530

    Sealed and Static class Example

---
+++ OopsConcept/SealedClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class BaseClass
+    {
+
+        public void DisplayBaseInfo()
+        {
+            Console.WriteLine("This is a Base Class");
+        }
+    }
+    sealed class DerivedClass : BaseClass
+    {
+        public void DisplayDerivedInfo()
+        {
+            Console.WriteLine("This is a Derived Class ");
+        }
+    }
+    internal class SealedClass
+    {
+        static void Main()
+        {
+            DerivedClass dc = new DerivedClass();
+            dc.DisplayBaseInfo();
+            dc.DisplayDerivedInfo();
+
+        }
+       
+    }
+}
---
+++ OopsConcept/StaticClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    static class CatWorld
+    {
+        public static int NumberOfCats;
+        public static int TotalCatNaps;
+
+        static CatWorld()
+        {
+            NumberOfCats = 5;
+            TotalCatNaps = 3;
+        }
+        public static void AddCats()
+        {
+            NumberOfCats++;
+            Console.WriteLine($"A New Cat Has Joined:{NumberOfCats}");
+        }
+        public static void AddNap()
+        {
+            TotalCatNaps++;
+            Console.WriteLine($"Total Number of Cat Naps:{TotalCatNaps}");
+        }
+    }
+    internal class StaticClass
+    {
+        static void Main()
+        {
+            CatWorld.AddCats();
+            CatWorld.AddNap();
+
+        }
+    }
+}
