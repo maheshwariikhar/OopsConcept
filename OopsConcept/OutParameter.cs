e38dd58e97fd7fa494d7f353f83f514adfb7f52b
Maheshwari Ikhar <maheshwariikhar@gmail.com>
2025-08-11 20:31:10 +0530

    Ref and out Parameter

---
+++ OopsConcept/OutParameter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class OutParameter
+    {
+        // method with two parameter
+        static void InitializeValues(out int number, out string str)
+        {
+            number = 22;
+            str = "Mahi";
+        }
+        static void Main()
+        {
+            //calling method with out para
+            InitializeValues(out int number, out string str);
+            Console.WriteLine("Out Parameter Value:"+ number );
+            Console.WriteLine("Out Parameter String:" + str);
+        }
+    }
+}
---
+++ OopsConcept/RefParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class RefParameter
+    {
+        static void IncrementValue(ref int number)
+        {
+            number++;
+        }
+        static void Main()
+        {
+            int incnumber = 5;
+            Console.WriteLine("Befor Increment Ref Parameter:" + incnumber);
+
+            // Calling method
+            IncrementValue(ref incnumber);
+            Console.WriteLine("After Increment Ref Parameter:" + incnumber);
+        }
+
+    }
+}
